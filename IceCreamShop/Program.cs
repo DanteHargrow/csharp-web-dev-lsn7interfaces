b992afc78284e1746ad9cc28a4b41f8e9aa84fcc
dantehargrow <dantehargrow@gmail.com>
2020-10-21 22:53:37 -0500

    Class 7 exercises

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace IceCreamShop
 {
@@ -11,13 +12,38 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            /*
+            foreach(Flavor item in availableFlavors)
+            {
+                Console.WriteLine(item.Name);
+            }
+            */
+            //FlavorComparer comparer = new FlavorComparer();
+            availableFlavors.Sort(new FlavorComparer());
+            availableCones.Sort(new ConeComparer());
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            /*
+            Console.WriteLine("\n");
+            foreach (Flavor item in availableFlavors)
+            {
+                Console.WriteLine(item.Name);
+            }*/
+
+            foreach (Cone item in availableCones)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
         }
     }
 }
